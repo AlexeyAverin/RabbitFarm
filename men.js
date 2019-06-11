8576cf154a6e5e6222f2d03156a385bd2ac6229b
Юрист <bercana.mail.ru>
2019-06-11 11:37:54 +0300

    Добрый день! Доработка, добавление полупрозрачного фона под всплывающим окном Уверенны ли Вы...

--- men.js
+++ men.js
@@ -14,25 +14,40 @@ document.body.addEventListener("click", function(event){
     if (event.target.className == "erase-rabbit" ) {//alert('Добрый день!!!' + rabbitid);
 
         
-        event.target.style.position = 'absolute';
-        event.target.style.top = 'calc(50vh - 50px)';
-        event.target.style.left = 'calc(50vw - 250px)';
-        event.target.style.width = '500px';
-        event.target.style.height = '90px';
+        // Полупрозрачный фон
+        var divWindow = document.createElement('div');
+        event.target.appendChild(divWindow);
+        divWindow.style.position = 'absolute';
+        divWindow.style.top = '0px';
+        divWindow.style.left = '0px';
 
-        event.target.style.textAlign = 'center';
-        event.target.style.padding = '30px';
-        event.target.style.border = '1px solid orange';
-        event.target.style.borderRadius = '30px';
-        event.target.style.boxShadow = '10px 10px 10px rgb(230, 230, 230)';
-        event.target.style.backgroundColor = 'white';
-        //event.target.style.visibility = 'hidden';
+        divWindow.style.width = '100vw';
+        divWindow.style.height = '100vh';
 
-        var rabbitid = event.target.getAttribute('rabbitid');
- 
+        //divWindow.style.visibility = 'hidden';
+        divWindow.style.backgroundColor = 'rgba(254, 254, 254, 0.7)';
 
-        event.target.innerHTML= 'Добрый день, Вы точно хотите удалить запись<p><a href="index.php?rabbitid=' + rabbitid + '&action=del">Да</a><a href="index.php">Нет</a></p>';
 
+
+        // Меню
+        var divMenu = document.createElement('div');
+        event.target.appendChild(divMenu);
+        divMenu.style.position = 'absolute';
+        divMenu.style.top = 'calc((100vh - 150px)/2)';
+        divMenu.style.left = 'calc((100vw - 560px)/2)';
+        divMenu.style.width = '500px';
+        divMenu.style.height = '90px';
+
+        divMenu.style.textAlign = 'center';
+        divMenu.style.padding = '30px';
+        divMenu.style.border = '1px solid orange';
+        divMenu.style.borderRadius = '30px';
+        divMenu.style.boxShadow = '10px 10px 10px rgb(230, 230, 230)';
+        divMenu.style.backgroundColor = 'white';
+        //divMenu.style.visibility = 'hidden';
+
+        var rabbitid = divMenu.getAttribute('rabbitid');
+        divMenu.innerHTML= 'Добрый день, Вы точно хотите удалить запись<p><a href="index.php?rabbitid=' + rabbitid + '&action=del">Да</a><a href="index.php">Нет</a></p>';
     }
 });
 
