e0cf095330dc743f8bd81c03b10347ddc518513b
Юрист <bercana.mail.ru>
2019-06-14 11:08:01 +0300

    Добрый деньgit add secret.php! Добавление данных mysql для подключения к базеgit add secret.php!

--- secret.php
+++ secret.php
@@ -6,4 +6,10 @@
 $mail_user = 'yvp777';
 $mail_pass = 'Mail@YvP';
 
+// MySQL
+$mysql_node   = 'localhost';
+$mysql_dbase  = 'rabbitfarm';
+$mysql_user   = 'farmer';
+$mysql_passwd = 'farmer575';
+
 ?>
