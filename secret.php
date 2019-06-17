6861e1e52e8634a259994905b25266f6ee5fbd61
Юрист <bercana.mail.ru>
2019-06-17 14:52:38 +0300

    Добрый день! В виде массива!

--- secret.php
+++ secret.php
@@ -7,9 +7,9 @@ $mail_user = 'yvp777';
 $mail_pass = 'Mail@YvP';
 
 // MySQL
-$mysql_node   = 'localhost';
-$mysql_dbase  = 'rabbitfarm';
-$mysql_user   = 'farmer';
-$mysql_passwd = 'farmer575';
+$mysql = array ('node'   => 'localhost',
+                'dbase'  => 'rabbitfarm',
+                'user'   => 'farmer',
+                'passwd' => 'farmer575');
 
 ?>
