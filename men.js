08da162ea905ad62d6a37b16ccdfd36e413324f9
Юрист <bercana.mail.ru>
2019-07-05 09:27:45 +0300

    Добрый деньgit add men.js rabbits.css! Изменение подключение свойств в виде класса, а не отдельных свойствgit add men.js rabbits.css!

--- men.js
+++ men.js
@@ -17,34 +17,13 @@ document.body.addEventListener("click", function(event){
         // Полупрозрачный фон
         var divWindow = document.createElement('div');
         event.target.appendChild(divWindow);
-        divWindow.style.position = 'absolute';
-        divWindow.style.top = '0px';
-        divWindow.style.left = '0px';
-
-        divWindow.style.width = '100vw';
-        divWindow.style.height = '100vh';
-
-        //divWindow.style.visibility = 'hidden';
-        divWindow.style.backgroundColor = 'rgba(254, 254, 254, 0.7)';
-
+        divWindow.classList.add("window"); 
 
 
         // Меню
         var divMenu = document.createElement('div');
         event.target.appendChild(divMenu);
-        divMenu.style.position = 'absolute';
-        divMenu.style.top = 'calc((100vh - 150px)/2)';
-        divMenu.style.left = 'calc((100vw - 560px)/2)';
-        divMenu.style.width = '500px';
-        divMenu.style.height = '90px';
-
-        divMenu.style.textAlign = 'center';
-        divMenu.style.padding = '30px';
-        divMenu.style.border = '1px solid orange';
-        divMenu.style.borderRadius = '30px';
-        divMenu.style.boxShadow = '10px 10px 10px rgb(230, 230, 230)';
-        divMenu.style.backgroundColor = 'white';
-        //divMenu.style.visibility = 'hidden';
+        divMenu.classList.add("menu");
 
         var id = event.target.getAttribute('id');
         var str = event.target.getAttribute('str');
@@ -55,7 +34,7 @@ document.body.addEventListener("click", function(event){
             var stringsrt = 'str=' + str + '&';
         }
 
-        divMenu.innerHTML= 'Добрый день, Вы точно хотите удалить запись<p><a href="index.php?' + stringsrt + 'id=' + id + '&action=del">Да</a><a href="index.php">Нет</a></p>';
+        divMenu.innerHTML= '<div>Добрый день, Вы точно хотите удалить запись<p><a href="index.php?' + stringsrt + 'id=' + id + '&action=del">Да</a><a href="index.php">Нет</a></p></div>';
     }
 
     if (event.target.id == "crtbre" ) {
@@ -65,28 +44,14 @@ document.body.addEventListener("click", function(event){
         var parentDiv = event.target.parentNode;
         var divWindow = document.createElement("div");
         parentDiv.appendChild(divWindow);
-        divWindow.style.position = 'absolute';
-        divWindow.style.top = '0px';
-        divWindow.style.left = '0px'
-        divWindow.style.width = '100vw';
-        divWindow.style.height = '100vh';
-        divWindow.style.backgroundColor = 'rgba(254, 254, 254, 0.7)';
+        divWindow.classList.add("window");
 
         var divMenu = document.createElement('div');
         parentDiv.appendChild(divMenu);
-        divMenu.style.position = 'absolute';
-        divMenu.style.boxSizing = 'border-box';
-
-        divMenu.style.width = '630px';
-        divMenu.style.height = '420px';
-        divMenu.style.top = 'calc((100hv - 420px)/2)';
-        divMenu.style.left = 'calc((100vw - 630px)/2)';
-        divMenu.style.border = '1px solid orange';
-        divMenu.style.borderRadius = '30px';
-        divMenu.style.backgroundColor = 'white';
+        divMenu.classList.add("menu");
 
 
-        divMenu.innerHTML = '<div style="padding: 30px;">Добрый день, Вы точно хотите из данной случки создать окрол, если да тогда заполните поля и нажмите "Создать", если нет нажмите "Отменить"</div><div><form method="GET" action="index.php" enctype="application/x-www-form-urlncoded"><input name="couplingId" value="' + couplingid + '" type="hidden"><input value="' + breedingDate + '" type="date"><input min="0" name="numberall" value="0" type="number"><input min="0" name="numberlive" value="0" type="number"></div><div><input type="submit" value="Создать"><input type="button" value="Отменить"></form></div>'
+        divMenu.innerHTML = '<div>Добрый день, <br>если Вы хотите из данной случки создать окрол, тогда заполните поля и нажмите "Создать", если нет нажмите "Отменить"</div><div><form method="GET" action="index.php" enctype="application/x-www-form-urlncoded"><input name="couplingId" value="' + couplingid + '" type="hidden"><input value="' + breedingDate + '" type="date"><input min="0" name="numberall" value="0" type="number"><input min="0" name="numberlive" value="0" type="number"></div><div><input type="submit" value="Создать"><input type="button" value="Отменить"></form></div>'
 
     }
 
