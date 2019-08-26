a424a02a820defba51a75f29eb21df172f43f532
Юрист <bercana.mail.ru>
2019-08-26 13:10:28 +0300

    Добрый деньgit add classes/Injection.php! Создали для injection новый класс Injectiongit add classes/Injection.php!

--- classes/Injection.php
+++ classes/Injection.php
@@ -5,10 +5,28 @@
 class Injection {
     public $injectionid, $injectiontype, $injectiondate, $injectionfinish, $id, $breedingid, $injectionstatus;
 
-    public function __construct() {
+    //public function __construct($injectionid, $injectiontype, $injectiondate, $injectionfinish, $id, $breedingid, $injectionstatus) {
         
-    }
+    //}
 
+
+
+
+
+
+    public function getHTML(){
+        $injection_sign = $this->injectionstatus == 'on' ? '&#10004;' : '';
+        return "<tr>
+                <td><a href='index.php?str=inj&action=mod&id='{$this->injectionid}'>{$this->injectionid}</a></td>
+                <td>{$this->injectiontype}</td>
+                <td>{$this->injectiondate}</td>
+
+                <td>{$this->injectionfinish}</td>
+                <td>{$this->id}</td>
+                <td>{$this->breedingid}</td>
+                <td>{$injection_sign}</td>
+                </tr>";
+    }
 }
 
 
