5e47a54e95eff8cb16bb19c28a6aeb0b552925d4
Юрист <bercana.mail.ru>
2019-08-26 10:50:02 +0300

    Доброе утроgit add classes/Injection.php! classInjectiongit add classes/Injection.php!

---
+++ classes/Injection.php
@@ -0,0 +1,17 @@
+<?php
+
+
+
+class Injection {
+    public $injectionid, $injectiontype, $injectiondate, $injectionfinish, $id, $breedingid, $injectionstatus;
+
+    public function __construct() {
+        
+    }
+
+}
+
+
+
+
+?>
