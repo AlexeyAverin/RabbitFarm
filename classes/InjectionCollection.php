d7cb40d49db061e44564eeb07a70ff3c31b3c18a
Юрист <bercana.mail.ru>
2019-08-27 13:47:17 +0300

    Добрый деньgit add classes/InjectionCollection.php! InjectionCollection

---
+++ classes/InjectionCollection.php
@@ -0,0 +1,24 @@
+<?php
+
+
+
+class InjectionCollection {
+    //CONST ACTION_UPD = 'upd';
+    //CONST ACTION_INS = 'ins';
+    //CONST STR_INJ = 'inj';
+    //public $injectionid, $injectiontype, $injectiondate, $injectionfinish, $id, $breedingid, $injectionstatus;
+
+    //public function __construct($injectionid, $injectiontype, $injectiondate, $injectionfinish, $id, $breedingid, $injectionstatus) {
+        
+    //}
+
+
+
+    static public function getNewForm($mens_womens, $injections_arr){
+        /** Форма новой Вакцины */
+    
+    }
+
+}
+
+?>
